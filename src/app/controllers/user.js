2952c55bb9614c8fb4a7ba8e968127cc6bd87c69
nghuuphuoc <phuoc@huuphuoc.me>
2013-12-30 09:15:45 +0700

    Implement listing users page

--- src/app/controllers/user.js
+++ src/app/controllers/user.js
@@ -1,5 +1,46 @@
+var mongoose = require('mongoose'),
+    User     = mongoose.model('user');
+
+/**
+ * List users
+ */
 exports.index = function(req, res) {
-    res.render('user/index', {
-        title: 'Users'
+    var perPage   = 10,
+        pageRange = 5,
+        page      = req.param('page') || 1,
+        q         = req.param('q') || '',
+        criteria  = q ? { username: new RegExp(q, 'i') } : {};
+
+    User.count(criteria, function(err, total) {
+        User.find(criteria).skip((page - 1) * perPage).limit(perPage).exec(function(err, users) {
+            if (err) {
+                users = [];
+            }
+
+            var numPages   = Math.ceil(total / perPage),
+                startRange = (page == 1) ? 1 : pageRange * Math.floor(page / pageRange) + 1,
+                endRange   = startRange + pageRange;
+
+            if (endRange > numPages) {
+                endRange = numPages;
+            }
+
+            res.render('user/index', {
+                title: 'Users',
+                req: req,
+                total: total,
+                users: users,
+
+                // Criteria
+                q: q,
+                currentUser: req.session.user.username,
+
+                // Pagination
+                page: page,
+                numPages: numPages,
+                startRange: startRange,
+                endRange: endRange
+            });
+        });
     });
 };
