a2b0606370abb3b23b4020fbe362fdfb689f3842
nghuuphuoc <phuoc@huuphuoc.me>
2014-01-03 10:13:11 +0700

    Fix: Show only activated posts in the homepage

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -5,16 +5,23 @@ var mongoose = require('mongoose'),
 
 exports.index = function(req, res) {
     var config = req.app.get('config');
-    Category.find({}).sort({ position: 1 }).exec(function(err, categories) {
-        Post.find().sort({ created_date: -1 }).limit(10).exec(function(err, posts) {
-            res.render('partial/posts', {
-                title: config.app.name,
-                categories: categories,
-                category: null,
-                moment: moment,
-                numPages: 1,
-                posts: posts
-            });
+    Category
+        .find({})
+        .sort({ position: 1 })
+        .exec(function(err, categories) {
+            Post
+                .find({ status: 'activated' })
+                .sort({ created_date: -1 })
+                .limit(10)
+                .exec(function(err, posts) {
+                    res.render('partial/posts', {
+                        title: config.app.name,
+                        categories: categories,
+                        category: null,
+                        moment: moment,
+                        numPages: 1,
+                        posts: posts
+                    });
+                });
         });
-    });
 };
