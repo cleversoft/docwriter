d8868c43f613924f7ae306a931ec61f540ce83cf
nghuuphuoc <phuoc@huuphuoc.me>
2014-04-18 10:14:22 +0700

    Show Disqus comments if enabled

--- src/config/config.js
+++ src/config/config.js
@@ -34,7 +34,7 @@ module.exports = {
         },
         comment: {
             disqus: {
-                shortName: 'bootstrapvalidator'
+                shortName: null
             }
         },
         mail: {
